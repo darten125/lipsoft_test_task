a7ba2d3fa566e2d1f1cd6261923f50067adc186b
darten125 <darten124@gmail.com>
2025-05-10 20:32:15 +0300

    mini fix

--- src/main/kotlin/Remote.kt
+++ src/main/kotlin/Remote.kt
@@ -3,34 +3,6 @@ package com.example
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GetUserTransactionsReceiveRemote(
-    val customerId: String,
-    val cursor: String? = null
-)
-
-@Serializable
-data class GetUserAccountTransactionsReceiveRemote(
-    val customerId: String,
-    val accountId: String,
-    val cursor: String? = null
-)
-
-@Serializable
-data class GetUserTransactionsByDateReceiveRemote(
-    val customerId: String,
-    val date: String,
-    val cursor: String? = null
-)
-
-@Serializable
-data class GetUserAccountTransactionsByDateReceiveRemote(
-    val customerId: String,
-    val accountId: String,
-    val date: String,
-    val cursor: String? = null
-)
-
-@Serializable
 data class TransactionRemote(
     val transactionId: String,
     val customerId: String,
